e890e7db7f9218301db1da080aa736d0c3a0b516
Vilmantas <vilmantas.sudaris@gmail.com>
2021-11-14 22:28:28 +0200

    [FIX] menu to close on click

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,7 +1,18 @@
 const navBtn = document.getElementById("hamburger-menu");
 const links = document.getElementById("navMenu");
+const navItems = document.querySelectorAll(".nav-item");
+const freeScreen = document.querySelectorAll("section");
 
-navBtn.addEventListener("click", function() {
+function toggleMenu() {
     links.classList.toggle("show-links");
     navBtn.classList.toggle("change");
-});
+}
+
+function closeMenu() {
+    links.classList.remove("show-links");
+    navBtn.classList.remove("change");
+}
+
+navBtn.addEventListener("click", toggleMenu);
+navItems.forEach(item => item.addEventListener("click", toggleMenu));
+freeScreen.forEach(item => item.addEventListener("click", closeMenu));
