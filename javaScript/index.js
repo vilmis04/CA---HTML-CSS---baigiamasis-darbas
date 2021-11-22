d6995987616d74b8c7fca7c26ea7dfaae265a8f4
Vilmantas <vilmantas.sudaris@gmail.com>
2021-11-22 21:13:59 +0200

    [upd] project card hover decor added

--- javaScript/index.js
+++ javaScript/index.js
@@ -16,19 +16,3 @@ function closeMenu() {
 navBtn.addEventListener("click", toggleMenu);
 navItems.forEach(item => item.addEventListener("click", closeMenu));
 freeScreen.forEach(item => item.addEventListener("click", closeMenu));
-
-
-
-// const projectCard = document.querySelectorAll(".project-card");
-// const cardDecor = document.querySelectorAll(".project-title-decor");
-//
-// function addDecor() {
-//     cardDecor.classList.add("show-links");
-// }
-//
-// function removeDecor() {
-//     cardDecor.classList.remove("show-links");
-// }
-//
-// freeScreen.forEach(item => item.addEventListener("mouseover", removeDecor));
-// projectCard.forEach(item => item.addEventListener("mouseover", addDecor));
